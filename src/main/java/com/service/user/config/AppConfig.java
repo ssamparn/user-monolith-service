30d0c07be7bf2c6b0c3c155eab1f1cab68fc5e30
samantrs <https://samantrs@git.rabobank.nl>
2020-11-27 07:04:44 +0530

    Security Config

--- src/main/java/com/service/user/config/AppConfig.java
+++ src/main/java/com/service/user/config/AppConfig.java
@@ -3,15 +3,9 @@ package com.service.user.config;
 import com.service.user.properties.UserServiceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public AppContext appContext() {
